cfd13519283c68c5e37555e57eaeb7290c8a674b
volkgrig <44508654+volkgrig@users.noreply.github.com>
2018-11-17 20:22:20 +0300

    Add files via upload

---
+++ Students.h
@@ -0,0 +1,27 @@
+/* Students.cpp */
+#pragma once
+#include <string>
+#include <iostream>
+
+class Students
+{
+private:
+	std::string name;
+	std::string group;
+	std::string NRB;
+	int         marks[4];
+public:
+	void set_name(std::string name1);
+	std::string get_name();
+	void set_group(std::string group1);
+	std::string get_group();
+	// NRB= NumberofRecordBook
+	void set_NumberofRecordBook(std::string NRB1);
+	std::string get_NumberofRecordBook();
+	void set_marks(int markspr[4]);
+	int get_marks_1();
+	int get_marks_2();
+	int get_marks_3();
+	int get_marks_4();
+	~Students();
+};
